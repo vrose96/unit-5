4a5ddfa069ba344c597f6bcc5f2e650cb33bedf1
Viviana Rosenbaum <vivianarosenbaum@gmail.com>
2024-07-15 17:55:35 -0600

    final commit

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import potentialCountriesReducer from "../redux/slices/potentialCountriesSlice";
+import displayedCountryReducer from "../redux/slices/displayCountrySlice";
+
+export default configureStore({
+    reducer: {
+        potentialCountries: potentialCountriesReducer,
+        displayedCountry: displayedCountryReducer,
+    },
+});
